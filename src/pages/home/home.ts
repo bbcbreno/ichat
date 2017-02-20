7ac13e6d8c4bef90858300b1f83cefd47ead4a05
Breno Caldeira <breno.caldeira@gmail.com>
2017-02-20 06:37:06 -0300

    makes basic chat with SendBird works

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { Events } from 'ionic-angular';
+
 
 import { NavController } from 'ionic-angular';
+// import { SendBird } from 'sendbird/SendBird.min.js';
+declare var SendBird: any;
 
 @Component({
   selector: 'page-home',
@@ -8,8 +12,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    sb = new SendBird({
+      appId: 'D6C50457-BEDE-405A-B282-2112D19ACFC2'
+    });
 
+  constructor(public navCtrl: NavController, public events: Events) {
+    var x = this.sb.connect('1', function(user, error) {
+      console.log("conectei?");
+      events.publish('sb:connect');
+    });
+
+    events.subscribe('sb:connect', () => {
+      this.sb.GroupChannel.createChannelWithUserIds(['1'], false, 'meucanal', 'meucanal', '1', function(channel, error) {
+          if (error) {
+              console.error(error);
+              return;
+          }
+
+          console.log(channel);
+      });
+    })
+    
   }
 
 }
---
+++ typings/index.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="globals/es6-shim/index.d.ts" />
+/// <reference path="modules/bcryptjs/index.d.ts" />
